ba79f6cc0e5404c29a9e0dd6c41929e027f5fbc7
eric03297071 <droopychicken1@gmail.com>
2023-12-26 20:58:54 -0600

    Complete

---
+++ Autoclicker.py
@@ -0,0 +1,28 @@
+from pynput import keyboard
+import mouse
+import time
+import threading
+
+keybind = keyboard.Key.end
+state = False
+
+def click():
+    while True:
+        if state:
+            print("autoclicker started")
+            mouse.click("left")
+        time.sleep(.01)
+        
+        
+def toggle_event(key):
+    if key == keybind:
+        global state
+        state = not state
+        
+        
+click_thread = threading.Thread(target=click)
+click_thread.start()
+
+# Collect events until released
+with keyboard.Listener(on_press=toggle_event) as listener:
+    listener.join()
